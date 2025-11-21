d2d761dfbf7f351ccb569f2073ec76d3933fd7d6
Helene T <helene.teychene@gmail.com>
2025-11-21 15:29:25 +0100

    feat/added buttons to allow users to rate each joke

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { getRandomJoke } from "./scripts/api-manager/services";
-
-import { showJoke } from "./scripts/jokes/ui";
+import { jokeReport, rateJoke } from "./scripts/jokes/score";
+import { disableScoreButtons, enableScoreButtons, showJoke } from "./scripts/jokes/ui";
 
 let currentJoke : {id: string; joke: string; status: number};
 
@@ -8,6 +8,7 @@ async function loadJoke() {
 const joke = await getRandomJoke();
 showJoke(joke);
 currentJoke = joke;
+enableScoreButtons();
 }
 
 loadJoke();
@@ -15,3 +16,12 @@ loadJoke();
 const jokeBtn = document.getElementById("joke-button") as HTMLButtonElement;
 jokeBtn.addEventListener("click", loadJoke);
 
+document.querySelectorAll(".score-button").forEach(button => {
+    const btn = button as HTMLButtonElement
+    const score = Number(btn.dataset.score);
+    btn.addEventListener("click", () => {
+        rateJoke(score, currentJoke);
+        console.log(jokeReport);
+        disableScoreButtons();
+});
+})
--- src/scripts/jokes/ui.ts
+++ src/scripts/jokes/ui.ts
@@ -2,3 +2,19 @@ export function showJoke(data :{id: string; joke: string; status: number}) {
     const jokeP = document.getElementById("joke-text") as HTMLParagraphElement;
     jokeP.textContent = data.joke;
 }
+
+export function disableScoreButtons() {
+    const scoreCtn = document.querySelectorAll(".score-button");
+    scoreCtn.forEach(button => {
+        const btn = button as HTMLButtonElement;
+        btn.disabled = true;
+    })
+}
+
+export function enableScoreButtons() {
+    const scoreCtn = document.querySelectorAll(".score-button");
+    scoreCtn.forEach(button => {
+        const btn = button as HTMLButtonElement;
+        btn.disabled = false;
+    })
+}
