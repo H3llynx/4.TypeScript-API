b74d6066fc3f9258a3ad3aaba8e6e4d8928c8968
Helene T <helene.teychene@gmail.com>
2025-11-21 17:27:42 +0100

    feat/added getCurrentWeather function that fetch current weather information from weatherstack API

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,19 @@
 import { getRandomJoke } from "./scripts/api-manager/joke-services";
-import { getLocationPermission } from "./scripts/api-manager/weather-services";
+import { getCurrentWeather, getLocationPermission } from "./scripts/api-manager/weather-services";
 import { jokeReport, rateJoke } from "./scripts/jokes/score";
 import { disableScoreButtons, enableScoreButtons, showJoke } from "./scripts/jokes/ui";
 
-let currentJoke : {id: string; joke: string; status: number};
+let currentJoke: { id: string; joke: string; status: number };
 
 async function loadJoke() {
-const joke = await getRandomJoke();
-showJoke(joke);
-currentJoke = joke;
-enableScoreButtons();
+    const joke = await getRandomJoke();
+    showJoke(joke);
+    currentJoke = joke;
+    enableScoreButtons();
 }
 
 loadJoke();
-getLocationPermission();
+getCurrentWeather();
 
 const jokeBtn = document.getElementById("joke-button") as HTMLButtonElement;
 jokeBtn.addEventListener("click", loadJoke);
@@ -25,7 +25,7 @@ document.querySelectorAll(".score-button").forEach(button => {
         rateJoke(score, currentJoke);
         console.log(jokeReport);
         disableScoreButtons();
-});
+    });
 })
 
 const allowLocationBtn = document.getElementById("position-permission") as HTMLButtonElement;
