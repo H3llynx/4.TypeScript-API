ba0a632b2a241cc0f52841e2d38ad537d2eeaf84
Helene T <helene.teychene@gmail.com>
2025-11-23 20:56:43 +0100

    fix / readapted currentJoke and loadJoke to adapt to the new joke object type (the one that allows ui update depending on the kind of joke

--- src/main.ts
+++ src/main.ts
@@ -4,17 +4,20 @@ import { jokeReport, rateJoke } from "./scripts/jokes/score";
 import { disableScoreButtons, enableScoreButtons, showJoke } from "./scripts/jokes/ui";
 import { showWeather } from "./scripts/weather/ui";
 
-let currentJoke: { id: string; joke: string; status: number };
+let currentJoke: { joke: string; type: string } | undefined;
 
 async function loadJoke() {
     const joke = await getRandomJoke();
-    showJoke(joke);
-    currentJoke = joke;
-    enableScoreButtons();
+    if (joke) {
+        showJoke(joke);
+        currentJoke = joke;
+        enableScoreButtons();
+    }
 }
 
 loadJoke();
-//const weatherData = localStorage.getItem("weather") || await getCurrentWeather();
+//const weatherData = await getCurrentWeather();
+//showWeather(weatherData.temperature, weatherData.src);
 showWeather(8, "https://i.ibb.co/Dm6y5cr/blobfish.webp");
 
 const jokeBtn = document.getElementById("joke-button") as HTMLButtonElement;
@@ -24,9 +27,11 @@ document.querySelectorAll(".score-button").forEach(button => {
     const btn = button as HTMLButtonElement
     const score = Number(btn.dataset.score);
     btn.addEventListener("click", () => {
-        rateJoke(score, currentJoke);
-        console.log(jokeReport);
-        disableScoreButtons();
+        if (currentJoke) {
+            rateJoke(score, currentJoke);
+            console.log(jokeReport);
+            disableScoreButtons();
+        }
     });
 })
 
