5a7bc8728aacafef4109e899d1cdceebf9ab1fd2
Helene T <helene.teychene@gmail.com>
2025-11-25 19:25:35 +0100

    feat / added the possibility fo r a user to change the score given to a joke

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { getRandomJoke } from "./scripts/api-manager/joke-services";
 import { getLocationPermission } from "./scripts/api-manager/weather-services";
-import { jokeReport, rateJoke } from "./scripts/jokes/score";
+import { jokeReport, scoreJoke } from "./scripts/jokes/score";
 import { disableScoreButtons, enableScoreButtons, showJoke } from "./scripts/jokes/ui";
 import { showMap } from "./scripts/map/ui";
 import { showWeather } from "./scripts/weather/ui";
@@ -8,40 +8,55 @@ import { showWeather } from "./scripts/weather/ui";
 
 let currentJoke: { joke: string; type: string } | undefined;
 
+function setScoreButtons() {
+    const scoreBtn = document.querySelectorAll(".score-button");
+    scoreBtn.forEach(button => {
+        const btn = button as HTMLButtonElement;
+        const score = Number(btn.dataset.score);
+        btn.addEventListener("click", () => {
+            if (currentJoke) {
+                scoreJoke(score, currentJoke);
+                console.log(jokeReport);
+                disableScoreButtons(score);
+            }
+        });
+    });
+}
 async function loadJoke() {
     const joke = await getRandomJoke();
     if (joke) {
         showJoke(joke);
         currentJoke = joke;
         enableScoreButtons();
+        setScoreButtons();
     }
-}
+};
 
 loadJoke();
 
+const scoreCtn = document.querySelector(".score-container") as HTMLDivElement;
+scoreCtn.addEventListener("click", (e: MouseEvent | TouchEvent) => {
+    const target = e.target as HTMLButtonElement;
+    if (!target || target.type !== "submit") return;
+    else {
+        jokeReport.pop();
+        enableScoreButtons();
+        setScoreButtons();
+    }
+})
+
 async function loadUserLocationInfo() {
     await getLocationPermission();
-    showWeather(8, "https://i.ibb.co/Dm6y5cr/blobfish.webp");
     showMap();
-    // const weatherData: { temperature: number; icon: string } | undefined = await getCurrentWeather();
-    // if (weatherData) {
-    //     showWeather(weatherData.temperature, weatherData.icon);
-    // }
+    //const weatherData: { temperature: number; icon: string } | undefined = await getCurrentWeather();
+    //if (weatherData) {
+    //    showWeather(weatherData.temperature, weatherData.icon);
+    //}
+    showWeather(8, "10n")
 }
 
 loadUserLocationInfo();
 
+
 const jokeBtn = document.getElementById("joke-button") as HTMLButtonElement;
 jokeBtn.addEventListener("click", loadJoke);
-
-document.querySelectorAll(".score-button").forEach(button => {
-    const btn = button as HTMLButtonElement
-    const score = Number(btn.dataset.score);
-    btn.addEventListener("click", () => {
-        if (currentJoke) {
-            rateJoke(score, currentJoke);
-            console.log(jokeReport);
-            disableScoreButtons();
-        }
-    });
-})
--- src/scripts/jokes/ui.ts
+++ src/scripts/jokes/ui.ts
@@ -1,24 +1,77 @@
+
 export function showJoke(data: { joke: string; type: string }) {
-    const jokeP = document.getElementById("joke-text") as HTMLParagraphElement;
-    const jokeCtn = document.getElementById("joke-container") as HTMLElement;
-    jokeP.textContent = data.joke;
-    if (data.type === "chuck") {
-        jokeCtn.classList.add("chuck");
-    } else jokeCtn.classList.remove("chuck");
+  const jokeP = document.getElementById("joke-text") as HTMLParagraphElement;
+  const jokeCtn = document.getElementById("joke-container") as HTMLElement;
+  jokeP.textContent = data.joke;
+  if (data.type === "chuck") {
+    jokeCtn.classList.add("chuck");
+  } else jokeCtn.classList.remove("chuck");
 }
 
-export function disableScoreButtons() {
-    const scoreCtn = document.querySelectorAll(".score-button");
-    scoreCtn.forEach(button => {
-        const btn = button as HTMLButtonElement;
-        btn.disabled = true;
-    })
-}
+export function disableScoreButtons(score: number) {
+  const scoreCtn = document.querySelector(".score-container") as HTMLDivElement;
+  scoreCtn.classList.add("scored");
+  scoreCtn.innerHTML = "<span>You scored this joke: </span>";
+  if (score === 1) {
+    scoreCtn.innerHTML += `<svg aria-label="1 star" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"
+            fill="#ffaa0d"><path
+              d="m320-240 160-122 160 122-60-198 160-114H544l-64-208-64 208H220l160 114-60 198ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z" />
+          </svg>`;
+  }
+  else if (score === 2) {
+    scoreCtn.innerHTML += `<svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"
+            fill="#ffaa0d"><path
+              d="m320-240 160-122 160 122-60-198 160-114H544l-64-208-64 208H220l160 114-60 198ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z" />
+          </svg>
+            <svg aria-label="2 stars" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"
+            fill="#ffaa0d"><path
+              d="m320-240 160-122 160 122-60-198 160-114H544l-64-208-64 208H220l160 114-60 198ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z" />
+          </svg>`;
+  } else {
+    scoreCtn.innerHTML += `<svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"
+            fill="#ffaa0d"><path
+              d="m320-240 160-122 160 122-60-198 160-114H544l-64-208-64 208H220l160 114-60 198ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z" />
+          </svg>
+            <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"
+            fill="#ffaa0d"><path
+              d="m320-240 160-122 160 122-60-198 160-114H544l-64-208-64 208H220l160 114-60 198ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z" />
+          </svg>
+            <svg aria-label="3 starq" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"
+            fill="#ffaa0d"><path
+              d="m320-240 160-122 160 122-60-198 160-114H544l-64-208-64 208H220l160 114-60 198ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z" />
+          </svg>
+            `;
+  };
+  scoreCtn.innerHTML += `<button id="update-score-button" tabindex="0" class="edit-btn" aria-label="Change score">
+  <svg class="edit" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#b3a576"><path d="M200-200h57l391-391-57-57-391 391v57Zm-80 80v-170l528-527q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L290-120H120Zm640-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z"/></svg>
+  </button>`;
+};
 
 export function enableScoreButtons() {
-    const scoreCtn = document.querySelectorAll(".score-button");
-    scoreCtn.forEach(button => {
-        const btn = button as HTMLButtonElement;
-        btn.disabled = false;
-    })
+  const scoreCtn = document.querySelector(".score-container") as HTMLDivElement;
+  scoreCtn.classList.remove("scored");
+  scoreCtn.innerHTML = `
+            <button tabindex="0" aria-label="score 3 stars" class="score-button" data-score="3">
+          <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"
+            fill="currentColor">
+            <path
+              d="m320-240 160-122 160 122-60-198 160-114H544l-64-208-64 208H220l160 114-60 198ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z" />
+          </svg></button>
+        <button tabindex="0" class="score-button" aria-label="score 2 stars" data-score="2"><svg aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor">
+            <path
+              d="m320-240 160-122 160 122-60-198 160-114H544l-64-208-64 208H220l160 114-60 198ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z" />
+          </svg></button>
+        <button tabindex="0" class="score-button" aria-label="score 1 star" data-score="1"><svg aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor">
+            <path
+              d="m320-240 160-122 160 122-60-198 160-114H544l-64-208-64 208H220l160 114-60 198ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z" />
+          </svg></button>
+        <span aria-hidden="true">Score this joke: </span>
+    `;
+  const scoreBtn = document.querySelectorAll(".score-button");
+  scoreBtn.forEach(button => {
+    const btn = button as HTMLButtonElement;
+    btn.disabled = false;
+  })
 }
