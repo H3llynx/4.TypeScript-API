56e641b30015dff727236e813bedd5bd5b5a88d0
Helene T <helene.teychene@gmail.com>
2025-11-25 19:25:35 +0100

    feat / added the possibility fo r a user to change the score given to a joke

--- src/scripts/jokes/score.ts
+++ src/scripts/jokes/score.ts
@@ -1,6 +1,6 @@
 export const jokeReport: { joke: string; score: number; date: string }[] = []
 
-export function rateJoke(value: number, joke: { joke: string; type: string }) {
+export function scoreJoke(value: number, joke: { joke: string; type: string }) {
     jokeReport.push({
         joke: joke.joke,
         score: value,
