27164cf8d8ae602aa200307b53ab00ca4ebe0778
Helene T <helene.teychene@gmail.com>
2025-11-28 15:00:16 +0100

    refactor/ removed alerts related to location permission and implemented specific widget ui when the user did not allow access to location

--- src/scripts/api-manager/weather-services.ts
+++ src/scripts/api-manager/weather-services.ts
@@ -1,22 +1,16 @@
+import type { Weather } from "../types/types";
+import { WeatherSchema } from "../types/zod-validation";
+
 export let userLocation: { latitude?: number; longitude?: number } = {}
 
 function getCurrentPosition(): Promise<GeolocationPosition> {
     return new Promise((resolve, reject) => {
         navigator.geolocation.getCurrentPosition(
             (position) => resolve(position),
-            (error) => {
-                if (error.code === 1) {
-                    alert("Location access denied. Please enable it in your browser settings.");
-                }
-                else if (error.code === 2) {
-                    alert("Location currently unavailable. Please try again later.")
-                }
-                else alert("Unknown error");
-                reject(error);
-            }
-        );
+            (error) => reject(error)
+        )
     });
-}
+};
 
 export async function getLocationPermission() {
     try {
@@ -25,9 +19,7 @@ export async function getLocationPermission() {
             const position = await getCurrentPosition();
             userLocation.latitude = position.coords.latitude;
             userLocation.longitude = position.coords.longitude;
-        } else if (result.state === "denied") {
-            alert("Location access denied. Please enable it in your browser settings.");
-        }
+        } else return;
     } catch (error) {
         console.log(error);
     }
@@ -36,18 +28,19 @@ export async function getLocationPermission() {
 const key = "8262f0260647636979f12177667c2539";
 const options = { method: 'GET', headers: { Accept: 'application/json' } };
 
-export async function getCurrentWeather() {
+export async function getCurrentWeather(): Promise<Weather> {
     const weatherURL = `https://api.openweathermap.org/data/2.5/weather?lat=${userLocation.latitude}&lon=${userLocation.longitude}&appid=${key}&units=metric`;
     try {
         const response = await fetch(weatherURL, options);
         const data = await response.json();
-        return (
-            {
-                temperature: data.main.temp,
-                icon: data.weather[0].icon
-            }
-        )
+        const validated = WeatherSchema.parse(data);
+        if (!data) throw new Error("Weather information unavailable");
+        return {
+            temperature: validated.main.temp,
+            icon: validated.weather[0].icon
+        };
     } catch (error) {
         console.error(error);
+        throw (error);
     }
 };
--- src/scripts/weather/ui.ts
+++ src/scripts/weather/ui.ts
@@ -1,6 +1,15 @@
-export function showWeather(temperature: number, icon: string) {
-    const widget = document.getElementById("temperature-widget") as HTMLDivElement
+import type { Weather } from "../types/types";
+const widget = document.getElementById("temperature-widget") as HTMLDivElement
+
+export function showWeather(data: Weather) {
     widget.innerHTML = `
-    <p>Temperature: ${temperature} °C</p> <img class=weather-icon src=https://openweathermap.org/img/wn/${icon}@2x.png>
-    `
+    <p>Temperature: ${data.temperature} °C</p> <img class=weather-icon src=https://openweathermap.org/img/wn/${data.icon}@2x.png>
+    `;
+}
+
+export function showWeatherUnavailable() {
+    widget.innerHTML = `
+    <img src="/src/assets/images/star.png" class="star">
+    <p class="no-location-p">Enable location in your browser settings to see the current weather information.</p>
+    `;
 }
