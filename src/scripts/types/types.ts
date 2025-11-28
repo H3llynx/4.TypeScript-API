54dae20993e2fb6f985d181ac267187f77b2dae1
Helene T <helene.teychene@gmail.com>
2025-11-28 14:58:59 +0100

    feat/ implemented zod for type validation of data retrieved from the jokes and weather APIs - added relevant tests

--- src/scripts/types/types.ts
+++ src/scripts/types/types.ts
@@ -1,10 +1,16 @@
+
 export interface Joke {
-    joke: string,
-    type: string
+    joke: string;
+    type: 'chuck' | 'dad';
 }
 
 export interface ScoredJoke {
     joke: string,
     score: number,
     date: string
+}
+
+export interface Weather {
+    temperature: number,
+    icon: string
 }
--- tests/services.test.js
+++ tests/services.test.js
@@ -1,7 +1,32 @@
 import { describe, expect, it } from "vitest";
 import { getRandomJoke } from "../src/scripts/api-manager/joke-services";
 import { getCurrentWeather } from "../src/scripts/api-manager/weather-services";
+import { ChuckSchema, DadSchema, WeatherSchema } from "../src/scripts/types/types";
 
+describe('API Schemas', () => {
+    it("Validates correct Chuck Norris joke type", () => {
+        const result = ChuckSchema.safeParse({ value: "Chuck test" });
+        expect(result.success).toBe(true);
+        expect(result.data).toEqual({ value: "Chuck test" });
+    });
+
+    it("rejects invalid Chuck joke type", () => {
+        const result = ChuckSchema.safeParse({ joke: "wrong" });
+        expect(result.success).toBe(false);
+    });
+
+    it("Validates correct Dad joke type", () => {
+        const result = DadSchema.safeParse({ joke: "Dad test" });
+        expect(result.success).toBe(true);
+    });
+    it("Validates correct Weather data types", () => {
+        const result = WeatherSchema.safeParse({
+            main: { temp: 0 },
+            weather: [{ icon: "0dh" }]
+        });
+        expect(result.success).toBe(true);
+    });
+});
 
 describe("getRandomJoke Test", () => {
     it("should be declared", () => {
